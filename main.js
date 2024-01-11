88769e4b1733d0e8cbe9bcf1e0efa957c2841517
Aditya Yadav <156325599+adityayadav8294@users.noreply.github.com>
2024-01-11 18:59:55 +0530

    Aditya
    i am a web developer

---
+++ main.js
@@ -0,0 +1,13 @@
+// toggle icon navbar 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/*================================== remove toggle icon and navbar when click navbar link (scroll) ==============================*/
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
